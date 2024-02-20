49b4c574eae41427bb6144400ced51a6c1d96fc4
halwohamda <halwohamda@gmail.com>
2024-02-19 19:20:40 -0800

    Done

---
+++ app.js
@@ -0,0 +1,7 @@
+// const header = document.querySelector(".main-header");
+// const togglebtn = document.querySelector(".menu-icon");
+
+
+// togglebtn.addEventListener('click',(e)=>{
+//     header.classList.toggle('active')
+// });
